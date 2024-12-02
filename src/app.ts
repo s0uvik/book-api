49c199cec4459935b7d5941e12dfeec629de4a4b
Souvik <souviksarkar260123@gmail.com>
2024-12-02 14:03:29 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,16 @@ import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 import bookRouter from "./book/bookRoute";
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: config.frontendDomain,
+  })
+);
 
 // Middleware to parse JSON requests (optional but recommended)
 app.use(express.json());
